02846476506f4c82cfd3fea77573ee079f5df3a9
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-11-07 13:51:45 -0300

    refactor(ErrorDetail): change date to timestamp

--- src/main/kotlin/com/example/demo/exception/ErrorDetail.kt
+++ src/main/kotlin/com/example/demo/exception/ErrorDetail.kt
@@ -2,4 +2,4 @@ package com.example.demo.exception
 
 import java.util.*
 
-class ErrorDetail(val date: Date, val status: Int, val message: String, val path: String)
+class ErrorDetail(val timestamp: Date, val status: Int, val message: String, val path: String)
